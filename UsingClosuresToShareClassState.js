7d1a224887ed3e26b3fc5a73bbb541afa6e490f1
Kamila Zaeva <Kamila_Zaeva@epam.com>
2016-05-17 12:55:20 +0300

    edit formatting

--- UsingClosuresToShareClassState.js
+++ UsingClosuresToShareClassState.js
@@ -1,37 +1,37 @@
 var Cat = (function() {
-  var cats = [];
+	var cats = [];
 
-  function Cat(name, weight) {
-    if (arguments.length < 2) {
-      throw new Error('Cat should have name and weight');
-    }
+	function Cat(name, weight) {
+		if (arguments.length < 2) {
+			throw new Error('Cat should have name and weight');
+		}
 
-    Object.defineProperty(this, "weight", {
-      get: function() {
-        return weight;
-      },
-      set: function(newWeight) {
-        weight = newWeight;
-      }
-    });
+		Object.defineProperty(this, "weight", {
+			get: function() {
+				return weight;
+			},
+			set: function(newWeight) {
+				weight = newWeight;
+			}
+		});
 
-    Object.defineProperty(this, "name", {
-      get: function() {
-        return name;
-      },
-      set: function(newName) {
-        name = newName;
-      }
-    });
+		Object.defineProperty(this, "name", {
+			get: function() {
+				return name;
+			},
+			set: function(newName) {
+				name = newName;
+			}
+		});
 
-    cats.push(this);
-  }
+		cats.push(this);
+	}
 
-  Cat.averageWeight = function() {
-    return cats.reduce(function(totalWeight, curCat) {
-      return totalWeight + curCat.weight;
-    }, 0) / cats.length;
-  };
+	Cat.averageWeight = function() {
+		return cats.reduce(function(totalWeight, curCat) {
+			return totalWeight + curCat.weight;
+		}, 0) / cats.length;
+	};
 
-  return Cat;
+	return Cat;
 }()); 
