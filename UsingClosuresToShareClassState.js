afaab7c6354624f8d6e58e7913933a9924e74d79
Kamila Zaeva <Kamila_Zaeva@epam.com>
2016-05-17 12:53:58 +0300

    complete all without extra tasks

---
+++ UsingClosuresToShareClassState.js
@@ -0,0 +1,37 @@
+var Cat = (function() {
+  var cats = [];
+
+  function Cat(name, weight) {
+    if (arguments.length < 2) {
+      throw new Error('Cat should have name and weight');
+    }
+
+    Object.defineProperty(this, "weight", {
+      get: function() {
+        return weight;
+      },
+      set: function(newWeight) {
+        weight = newWeight;
+      }
+    });
+
+    Object.defineProperty(this, "name", {
+      get: function() {
+        return name;
+      },
+      set: function(newName) {
+        name = newName;
+      }
+    });
+
+    cats.push(this);
+  }
+
+  Cat.averageWeight = function() {
+    return cats.reduce(function(totalWeight, curCat) {
+      return totalWeight + curCat.weight;
+    }, 0) / cats.length;
+  };
+
+  return Cat;
+}()); 
