7c5814861e3fb6aaedb09a231e93491058320db1
Kamila Zaeva <Kamila_Zaeva@epam.com>
2016-05-18 16:43:37 +0300

    fixed

--- ClosuresScopes.js
+++ ClosuresScopes.js
@@ -1,11 +1,9 @@
 function createFunctions(n) {
 	var callbacks = [];
-	for (let i=0; i<n; i++) {
-		callbacks.push(function(x){
-			return function() {
-				return x;
-			}
-		}(i));
+	for (let i = 0; i < n; i++) {
+		callbacks.push(function(){
+			return i;
+		});
 	}
 	return callbacks;
 }
